95189b6a9a224e9190fd2e331c3be918189d44e6
victoreis2000@gmail.com <victoreis2000@gmail.com>
2021-10-09 18:32:05 -0300

    configuration dotenv

--- next.config.js
+++ next.config.js
@@ -1,2 +1,11 @@
 const withImages = require('next-images')
 module.exports = withImages()
+module.exports.env = {
+    AUTH_AOBARIZATION: process.env.AUTH_AOBARIZATION,
+    AUTH_AUTHORIZATION: process.env.AUTH_AUTHORIZATION,
+    AUTH_GRANT_TYPE: process.env.AUTH_GRANT_TYPE,
+    AUTH_PASSWORD: process.env.AUTH_PASSWORD,
+    AUTH_GRANT_TYPE_REFRESH: process.env.AUTH_GRANT_TYPE_REFRESH,
+    URL_IA_API: process.env.URL_IA_API,
+    URL_BACKEND: process.env.URL_BACKEND
+}
